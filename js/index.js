c6016ea284294dd665f80e73b025b598f755c721
alexander <guijaffe@mail.ru>
2025-01-10 19:09:25 +0300

    localStorage

--- js/index.js
+++ js/index.js
@@ -4,11 +4,16 @@ const modalConfirmed = () => {
 	const declineButton = document.getElementById('declineAge');
 	const pageContent = document.getElementById('pageContent');
 
-	modal.style.display = 'flex';
-
-	pageContent.style.display = 'none';
+	if (sessionStorage.getItem('ageConfirmed') === 'true') {
+		modal.style.display = 'none';
+		pageContent.style.display = 'block';
+	} else {
+		modal.style.display = 'flex';
+		pageContent.style.display = 'none';
+	}
 
 	confirmButton.onclick = function() {
+		sessionStorage.setItem('ageConfirmed', 'true');
 		modal.style.display = 'none';
 		pageContent.style.display = 'block';
 		console.log('Возраст подтвержден');
@@ -19,19 +24,24 @@ const modalConfirmed = () => {
 		window.location.href = 'https://www.disney.com/';
 		console.log('Возраст не подтвержден');
 	};
-}
+};
 
 const cookieWarning = () => {
-	document.addEventListener('DOMContentLoaded', function () {
-		const cookieNotice = document.getElementById('cookieNotice');
-		const acceptButton = document.getElementById('acceptCookies');
+	const cookieNotice = document.getElementById('cookieNotice');
+	const acceptButton = document.getElementById('acceptCookies');
 
-		acceptButton.addEventListener('click', function () {
-			cookieNotice.style.display = 'none';
-		});
+	if (sessionStorage.getItem('cookiesAccepted') === 'true') {
+		cookieNotice.style.display = 'none';
+	} else {
+		cookieNotice.style.display = 'flex';
+	}
+
+	acceptButton.addEventListener('click', function () {
+		sessionStorage.setItem('cookiesAccepted', 'true');
+		cookieNotice.style.display = 'none';
+		console.log('Cookies приняты');
 	});
-}
+};
 
-
-modalConfirmed()
-cookieWarning()
+modalConfirmed();
+cookieWarning();
